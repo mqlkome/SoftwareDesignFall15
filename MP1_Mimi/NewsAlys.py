3ee9fb6d8ef3eee383419e781a652908e0e14097
Sarah Walters <sarah.walters@students.olin.edu>
2015-10-02 09:10:43 -0400

    Feedback on MP1

--- MP1_Mimi/NewsAlys.py
+++ MP1_Mimi/NewsAlys.py
@@ -1,7 +1,7 @@
 from pattern.web import *
 from pattern.en import *
-#Loopsites takes a user-inputted word, creates a gsearch query for each site, and runs gsearch on each query. 
-#It gives the URL from gsearch to artext, which grabs the plaintext article from that URL. 
+#Loopsites -- what's loopsites? did you rename it? -- takes a user-inputted word, creates a gsearch query for each site, and runs gsearch on each query.
+#It gives the URL from gsearch to artext, which grabs the plaintext article from that URL.
 #Then, it loops through the articles from artext, and runs sentmood on each of them.
 
 print 'Thanks for opening NewsAlys. What would you like to search for? Please enter newsalys("your interest")'
@@ -9,19 +9,21 @@ def newsalys(userin):
      sites = ["www.huffingtonpost.com", "www.bbc.com", "www.latimes.com", "www.nytimes.com", "www.foxnews.com", "www.cnn.com"]
      print "Here are some articles about " + userin
      for site in sites:
-          url = gsearch("site:" + site + " " + userin) 
-          article = artext(url) 
+          # I went and figured out how to do string interpolation with 2 arguments after our conversation:
+          # test = "test string numbers %s %s" % (1,2)
+          # The way you're doing it here is completely valid too!
+          url = gsearch("site:" + site + " " + userin)
+          article = artext(url)
           analysis = sentmood(article)
           print url
           print analysis
      print "Indicative means facts and beliefs. Imperative means commands and warnings. Conditional means conjectures. Subjunctive means wishes and opinions."
 
-##The functions gsearch and artext are here to search for userinput on the web and process the search results into something that can be analysed.
-# gsearch is directed to run a google search of a news site and return the url of the first article on userinput. 
+##The functions gsearch and artext are here to search for userinput (query?) on the web and process the search results into something that can be analysed.
+# gsearch is directed to run a google search of a news site and return the url of the first article on userinput (query?).
 def gsearch(query):
      g = Google()
      return g.search(query)[0].url
-
 
 # loopsites provides a url(from gsearch) and artext makes a string: the plaintext of the article at the URL.
 def artext(url):
@@ -31,12 +33,13 @@ def artext(url):
 
 ##Below are the functions that perform the analysis of the plaintext of the articles.
 ## They analyse the subjectivity and mood of each article and return descriptive words.
-##Sentmood uses positivity and subjectivity to turn sentiment and mood analysis into user-friendly descriptors. 
-def sentmood(article):            
+##Sentmood uses positivity and subjectivity to turn sentiment and mood analysis into user-friendly descriptors.
+def sentmood(article):
      sent = sentiment(article) ## Will return a tuple, (polarity, subjectivity)
      subj = subjectivity(sent[1])
      pos = positivity(sent[0])
      atti = mood(article) ## will return a string which indicates the grammatical mood (indicative, imperative, conditional, subjunctive)
+     # -> documentation consistency -- (polarity, subjectivity) is -actually- a tuple, (indicative, imperative...) is just a list of possibilities...but looks like a tuple
 
      return [subj, pos, atti]
 
@@ -48,7 +51,7 @@ def positivity(sent0):
      elif -1 <= sent0 <= -0.1:
           return "Negative"
      else:
-          return
+          return # Great that you're catching the error case!
 
 def subjectivity(sent1):
      if 0 <= sent1 < .5:
@@ -57,8 +60,3 @@ def subjectivity(sent1):
           return "Subjective"
      else:
           return
-
-	
-
-	
-
